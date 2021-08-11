3c6c2da2304421c85f033c7d1e2972232c1660b9
Brian Rodas <>
2021-08-11 13:42:04 -0600

    se agrgo las capas de la tabla Fabriante

---
+++ src/main/java/com/curlp/datos/CDFabricante.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.curlp.datos;
+
+import com.curlp.logica.CLFabricante;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Luis
+ */
+public class CDFabricante {
+    
+    // Declarar las variables de conexion y consulta
+    private final Connection cn;
+    PreparedStatement ps;
+    ResultSet rs;
+    Statement st;
+
+    public CDFabricante() throws SQLException{
+        this.cn = Conexion.conectar();
+    }
+
+    //Metodos para insertar un fabricante
+    public void insertarFabricante(CLFabricante cl)throws SQLException {
+        String sql = "{CALL insertarFabricante(?)}";
+        
+        try {
+            ps = cn.prepareCall(sql);
+            ps.setString(1, cl.getNombreFabricante());
+            ps.execute();
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Error" + e.getMessage());
+        }
+    }
+    //Metodo para actualizar el fabricante en la tabla
+    public void actualizarFabricante(CLFabricante cl)throws SQLException {
+        String sql = "{CALL actualizarFabricante(?,?)}";
+        
+        try {
+            ps = cn.prepareCall(sql);
+            ps.setInt(1, cl.getIdFabricante());
+            ps.setString(2, cl.getNombreFabricante());
+            ps.execute();
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Error" + e.getMessage());
+        }
+    }
+    //Metodo para eliminar el fabricante en la tabla
+    public void eliminarFabricante(CLFabricante cl)throws SQLException {
+        String sql = "{CALL eliminarFabricante(?)}";
+        
+        try {
+            ps = cn.prepareCall(sql);
+            ps.setInt(1, cl.getIdFabricante());
+            ps.execute();
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Error" + e.getMessage());
+        }
+    }
+    
+    //Metodo para obtener el id de el Fabricante
+}
---
+++ src/main/java/com/curlp/logica/CLFabricante.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.curlp.logica;
+
+/**
+ *
+ * @author Luis
+ */
+public class CLFabricante {
+    private int idFabricante;
+    private String nombreFabricante;
+
+    public int getIdFabricante() {
+        return idFabricante;
+    }
+
+    public void setIdFabricante(int idFabricante) {
+        this.idFabricante = idFabricante;
+    }
+
+    public String getNombreFabricante() {
+        return nombreFabricante;
+    }
+
+    public void setNombreFabricante(String nombreFabricante) {
+        this.nombreFabricante = nombreFabricante;
+    }
+    
+    
+}
