5a680a321a7ab2fd07309763e3188ca9eab2e18a
usuario <usuario@DESKTOP-GSKLQGP>
2021-08-11 13:28:58 -0600

    agregar capalogica establecimiento

---
+++ src/main/java/com/curlp/logica/CLEstablecimiento.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.curlp.logica;
+
+/**
+ *
+ * @author uuario
+ */
+public class CLEstablecimiento {
+
+    private int codEstablecimiento;
+    private String nombreEstablecimiento;
+
+    public int getCodEstablecimiento() {
+        return codEstablecimiento;
+    }
+
+    public void setCodEstablecimiento(int codEstablecimiento) {
+        this.codEstablecimiento = codEstablecimiento;
+    }
+
+    public String getNombreEstablecimiento() {
+        return nombreEstablecimiento;
+    }
+
+    public void setNombreEstablecimiento(String nombreEstablecimiento) {
+        this.nombreEstablecimiento = nombreEstablecimiento;
+    }
+
+}
